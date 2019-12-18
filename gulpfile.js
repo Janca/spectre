a849b92f5df7c14d15b14325dec5637d3fcb4fed
Janca <tejanca.93@gmail.com>
2019-12-17 20:21:06 -0600

    Cleaned gulpfile. Added 'natives' to dev pkgs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,57 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleancss = require('gulp-clean-css');
-var csscomb = require('gulp-csscomb');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var pug = require('gulp-pug');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleancss = require('gulp-clean-css');
+const csscomb = require('gulp-csscomb');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
 
-var paths = {
-  source: './src/*.scss',
-  doc: './docs/src/scss/*.scss'
+const p = {
+    sass: {
+        src: './src/*.scss',
+        dst: './dist'
+    },
+    docPug: {
+        src: './docs/src/**/!(_)*.pug',
+        dst: './docs'
+    },
+    docSass: {
+        src: './docs/src/scss/*.scss',
+        dst: './docs/dist'
+    }
 };
 
-gulp.task('watch', function() {
-  gulp.watch('./**/*.scss', ['build']);
-  gulp.watch('./**/*.scss', ['docs']);
-  gulp.watch('./**/*.pug', ['docs']);
+const buildStyle = (src, dst) => {
+    gulp.src(src)
+        .pipe(sass({outputStyle: 'compact', precision: 10}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(csscomb())
+        .pipe(gulp.dest(dst))
+        .pipe(cleancss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(dst));
+};
+
+const buildPug = (src, dst) => {
+    gulp.src(src)
+        .pipe(pug({pretty: true, doctype: 'html'}))
+        .pipe(gulp.dest(dst));
+};
+
+gulp.task('watch', function () {
+    gulp.watch('./**/*.scss', ['build']);
+    gulp.watch('./**/*.scss', ['docs']);
+    gulp.watch('./**/*.pug', ['docs']);
 });
 
-gulp.task('build', function() {
-  gulp.src(paths.source)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist'));
+gulp.task('build', function () {
+    buildStyle(p.sass.src, p.sass.dst);
 });
 
-gulp.task('docs', function() {
-  gulp.src(paths.doc)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
-  gulp.src(paths.source)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
-  gulp.src('docs/src/**/!(_)*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./docs/'));
+gulp.task('docs', function () {
+    buildStyle(p.docSass.src, p.docSass.dst);
+    buildStyle(p.sass.src, p.docSass.dst);
+    buildPug(p.docPug.src, p.docPug.dst);
 });
 
 gulp.task('default', ['build']);
